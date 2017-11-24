9f796fc090ff1d52fff2812db0f1ea51e4941350
sunlandong <sunlandong@9086.cn>
2017-11-24 18:02:45 +0800

    获取url参数

--- lib/Utils.ts
+++ lib/Utils.ts
@@ -1,11 +1,28 @@
 
 
-export const UUID = () => {
+
+const UUID = () => {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
-          .toString(16)
-          .substring(1);
-      }
+            .toString(16)
+            .substring(1);
+    }
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+        s4() + '-' + s4() + s4() + s4();
+}
+
+const getQuertString = (key: string) => {
+    let qs = location.search.substr(1), // 获取url中"?"符后的字串   
+        items = qs.length ? qs.split("&") : [] // 取得每一个参数项,
+    const item = items.filter(item => {
+        return item.split("=")[0] === key
+    })
+
+    return item[key];
+}
+
+
+export default {
+    UUID,
+    getQuertString
 }
